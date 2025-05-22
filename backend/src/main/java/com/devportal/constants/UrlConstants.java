3ad254c3979795bc82cb8fd4a37cf7af44da2e63
Nakul Mitra <nakulmitra2114@gmail.com>
2025-05-22 21:12:24 +0530

    created constant file

---
+++ backend/src/main/java/com/devportal/constants/UrlConstants.java
@@ -0,0 +1,9 @@
+package com.devportal.constants;
+
+public class UrlConstants {
+
+	public static final String RETURN_SUCCESS_MSG = "Data returned successfuly";
+	public static final String DATA_NOT_FOUND = "Data not found";
+	public static final String CREATE_SUCCESS_MSG = "Data created successfuly";
+
+}
