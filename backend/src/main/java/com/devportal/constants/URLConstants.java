b4b4f781a89d286a3532f911ebd99b9a2068f41c
Nakul Mitra <nakulmitra2114@gmail.com>
2025-05-23 19:17:08 +0530

    created constatnts

--- backend/src/main/java/com/devportal/constants/URLConstants.java
+++ backend/src/main/java/com/devportal/constants/URLConstants.java
@@ -1,9 +1,21 @@
 package com.devportal.constants;
 
-public class UrlConstants {
+public class URLConstants {
+
+	private URLConstants() {
+
+	}
+
+	public static final String PREFIX = "http://devportal.com/";
 
 	public static final String RETURN_SUCCESS_MSG = "Data returned successfuly";
 	public static final String DATA_NOT_FOUND = "Data not found";
 	public static final String CREATE_SUCCESS_MSG = "Data created successfuly";
+
+	public static final String SHORT_CODE = "shortCode";
+	public static final String ORIGINAL_URL = "originalUrl";
+	public static final String CREATED_AT = "createdAt";
+
+	public static final String ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 }
